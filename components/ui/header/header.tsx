1ff6f3bab86309b5bd38a58144c7b7d292b1afce
Nate May <nathaniel.p.may@gmail.com>
2025-03-25 11:39:12 -0700

    Add header

---
+++ components/ui/header/header.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../button";
+
+export function Header() {
+  return (
+    <header className="h-20 flex justify-between bg-black pr-5 pl-5 items-center">
+      <div>SYNTHASE studio</div>
+      <Button>Log In</Button>
+    </header>
+  );
+}
