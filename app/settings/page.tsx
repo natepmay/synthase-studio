7d04175c3951ad9c2e6897bc58255f7876ebeeb1
Nate May <nathaniel.p.may@gmail.com>
2025-03-26 22:30:20 -0700

    add shadcn form and deps

---
+++ app/settings/page.tsx
@@ -0,0 +1,9 @@
+export default function Settings() {
+  return (
+    <div className="w-full max-w-2xl">
+      <h2>Profile Settings</h2>
+      <hr />
+      <form>Display Name</form>
+    </div>
+  );
+}
