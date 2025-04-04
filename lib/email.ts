c739bc3582c3d3b5f7d3f5da236caf4669d50671
Nate May <nathaniel.p.may@gmail.com>
2025-04-04 16:20:07 -0700

    add sendEmail function connected to mailjet api

--- lib/email.ts
+++ lib/email.ts
@@ -1,3 +1,7 @@
+"use server";
+
+import MJ, { Client } from "node-mailjet";
+
 interface SendProps {
   from?: {
     name?: string;
@@ -10,24 +14,61 @@ interface SendProps {
   templateId?: number;
   variables?: Record<string, string>;
   subject?: string;
-  textPart?: string;
-  htmlPart?: string;
+  textBody?: string;
+  htmlBody?: string;
 }
 
-interface Email {
+interface Email<ClientType> {
   fromName: string | undefined;
   fromEmail: string | undefined;
-  send: (props: SendProps) => Object | Error;
+  client: ClientType;
+  send: (props: SendProps) => Promise<Object | Error>;
 }
 
-class MailJet implements Email {
+/**
+ * MailJet API wrapped in a custom class to respect the dependency inversion princple.
+ * If a new email service is used, try to make it implement the same
+ * Email<ClientType> interface.
+ */
+class MailJet implements Email<Client> {
   fromName;
   fromEmail;
+  client;
   constructor(fromName?: string, fromEmail?: string) {
+    console.log("Api key", process.env.MJ_APIKEY_PUBLIC);
+    const mailjet = MJ.apiConnect(
+      process.env.MJ_APIKEY_PUBLIC!,
+      process.env.MJ_APIKEY_PRIVATE!
+    );
     this.fromName = fromName;
     this.fromEmail = fromEmail;
+    this.client = mailjet;
   }
   send(props: SendProps) {
-    return {};
+    const processedProps = {
+      From: {
+        Name: props.from?.name ?? this.fromName,
+        Email: props.from?.email ?? this.fromEmail,
+      },
+      To: props.to.map((to) => ({
+        Name: to.name,
+        Email: to.email,
+      })),
+      TemplateID: props.templateId,
+      Variables: props.variables,
+      Subject: props.subject,
+      TextPart: props.textBody,
+      HTMLPart: props.htmlBody,
+    };
+    const response = this.client
+      .post("send", { version: "v3.1" })
+      .request({ Messages: [processedProps] });
+    return response;
   }
+}
+
+const emailClient = new MailJet("Synthase Studio", "test@natemay.dev");
+
+export async function sendEmail(props: SendProps) {
+  return emailClient.send(props);
 }
