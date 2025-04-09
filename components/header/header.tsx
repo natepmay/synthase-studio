5bbb5a8d4622c5158116e550db6796c8b668b5e7
Nate May <nathaniel.p.may@gmail.com>
2025-04-08 22:24:05 -0700

    user dropdown with settings page link

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,6 +4,15 @@ import { Button } from "../ui/button";
 import { UserContext } from "../providers/UserContext";
 import { useContext } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import Link from "next/link";
 
 export function Header() {
   const { user } = useContext(UserContext);
@@ -18,10 +27,24 @@ export function Header() {
     <header className="h-20 flex justify-between bg-black pr-5 pl-5 items-center">
       <div>SYNTHASE studio</div>
       <p>User: {user?.name}</p>
-      <Avatar>
-        <AvatarImage src={user?.image ?? undefined} alt={user?.name} />
-        <AvatarFallback className="text-black">{initials}</AvatarFallback>
-      </Avatar>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar>
+            <AvatarImage src={user?.image ?? undefined} alt={user?.name} />
+            <AvatarFallback className="text-black">{initials}</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="mr-5 w-56">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <Link href="/settings">
+            <DropdownMenuItem className="cursor-pointer">
+              Settings
+            </DropdownMenuItem>
+          </Link>
+          <DropdownMenuItem>Log Out</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </header>
   );
 }
